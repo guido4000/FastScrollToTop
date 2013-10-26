10b568c68f47339cd14519f279e78b9aca320569
guido4000 <guido4000@gmail.com>
2013-10-26 15:01:09 +0200

    initial commit

---
+++ bg.js
@@ -0,0 +1,9 @@
+// icon from http://www.iconarchive.com/show/origami-birds-icons-by-jozef89/bird-red-2-icon.html
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+	  // No tabs or host permissions needed!
+	  
+	  chrome.tabs.executeScript({
+	    code: 'scrollTo();'
+	  });
+	});
